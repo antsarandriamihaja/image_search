05ea99c8629c90bc39e1b4dc5b4c044e983c43fe
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-04-25 12:34:25 -0400

    GET and POST routes added

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,15 @@
+$("#query-form").on('submit', function(e){
+    e.preventDefault();
+})
+
 $('.btn-search').on('click', function(){
     $.ajax({
-        url:'/search/image',
+        url:'/api/imagesearch',
         type: 'POST',
         dataType: 'JSON',
         data: {url: $("#image-query").val()},
         success: function(data){
-            var result = 'json results here';
+            var result = 'results: '+data.query;
             $('#json-results').html(result);
         }
     })
--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const hbs = require('hbs');
 var app = express();
 var port = process.env.PORT || 3000;
 
 //config app
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'hbs');
 
 app.get('/', (req,res)=>{
+    console.log('get running');
     res.render(path.join(__dirname, '../index.hbs'));
 });
 
-app.post('/search/image', (req, res) =>{
-    var url = req.body.url;
-    res.send(url);
+app.post('/api/imagesearch', (req, res) =>{
+    var query = req.body.url;
+    res.send({"query":query});
 })
 
 
